ec6118a39a6576197603e9d263f8442f158f4e68
Destiny653 <fokundem.com@gmail.com>
2025-04-03 03:47:32 +0100

    route #

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
  // src/App.tsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Layout from './layout';
 import { ThemeProvider } from './context/ThemeContext';
